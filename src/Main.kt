98ad775568114f0740b5f6bb365cd4c3a0bec1ac
axmed-1 <akmed.mikailov@bk.ru>
2025-10-03 00:46:44 +0300

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,68 @@
+//шаги
+//fun main(){
+    /*  print("Hello world")
+      println("Today is sunny")
+
+      println("Тут очень большой текст,который у меня не поместится")
+
+      val city  = "Москва"
+      var temperature = 20
+      temperature +=5
+      const val PI = 3.1415
+      println(PI)
+      val count: Int = 2
+      println(count)
+      val game = "Dota2"
+      val year = 2005
+      val message = "$game - пользовательская карта для warcragt разрабатываемая с $year года"
+      println(message)
+     println("Hello world") \*
+
+     */
+    /*
+     val name = "Denis"
+     val age = 18
+     println("hello")
+     println("My name is $name")
+     println("My age is $age")
+      val a = 10
+      val b = 15
+      val c = a + b + 8
+      val str = c.toString()
+      println(str)
+      val numlist = arrayOf(1,2,3)
+      var x = 0
+      while (x<3){
+          println("item $x is $(numlist[x]")
+          x = x+ 1
+      val number: Double = 12.7
+      val IntegerNumber: Int = number.toInt()
+      println(IntegerNumber)
+
+      var x: Int = 65
+      var IsPunk = true
+      var message = "hello"
+     IsPunk = 10
+     var y = 15
+     y = y + "hello"
+
+     print("Hello")
+     print("kotlin")
+
+
+
+
+
+
+
+
+
+
+
+
+    *\
+
+     */
+
+
+
---
+++ src/Person.kt
@@ -0,0 +1,78 @@
+fun main(){
+    /*
+        val name = readln()
+        println("Ваше имя - $name")
+        print("Введите ваше имя")
+        val name = readln()
+        print("Введите ваш возраст")
+        val age = readln().toInt()
+        println("Через 5 лет вам будет $age + 5 лет")
+
+        val unreadCount = 5
+        val readCount = 100
+        println("У вас ${unreadCount + readCount} сообщений")
+
+        val a = 10
+        val b = 3
+        println("Сумма ${a + b}")
+        println("разность ${a - b}")
+        println("произведение ${a * b}")
+        println("частное ${a / b}")
+        println("остаток ${a % b}")
+
+        print("Введите первое число: ")
+        val num1 = readln().toDouble()
+
+        print("Введите второе число: ")
+        val num2 = readln().toDouble()
+
+        println("Результаты операций:")
+        println("$num1 + $num2 = ${num1 + num2}")
+        println("$num1 - $num2 = ${num1 - num2}")
+        println("$num1 * $num2 = ${num1 * num2}")
+        println("$num1 / $num2 = ${num1 / num2
+
+
+
+    \*
+
+     */
+    /*
+        var count: Int = 10
+        println(count)
+        var count: Int = 10
+        count = count + 1
+
+        println(count)
+
+        var count: Int = 10
+        count++
+        println(count)
+
+
+        var count: Int = 10
+        count++
+        count--
+        println(count)
+
+        val a = 5
+        val b = 2
+        println(a / b)
+        println(a%b)
+        val TotalSecond = readln().toInt()
+        val hours = TotalSecond / 3600
+        val minutes = TotalSecond % 3600 / 60
+        val seconds = TotalSecond %3600 % 60
+        println("$hours ч $minutes мин $seconds сек")
+        val fahrenheit = readln().toDouble()
+        val celsius = (fahrenheit - 32) * (5 / 9)
+        println("$fahrenheit F $celsius C")
+
+
+
+    \*
+
+     */
+
+
+}
